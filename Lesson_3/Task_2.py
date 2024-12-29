2b6bcf2684b745b360e1231445e0b0a0cd0f34bf
Margarita <maggie.rudenko@gmail.com>
2024-12-29 14:25:18 -0500

    HW for Lesson_3

---
+++ Lesson_3/Task_2.py
@@ -0,0 +1,7 @@
+list_1 = [12, 3, 4, 10]
+# list_1 = [1]
+# list_1 = []
+# list_1 = [12, 3, 4, 10, 8]
+if len(list_1) > 1:
+    list_1.insert(0, list_1.pop())
+print(list_1)
---
+++ Lesson_3/Task_3.py
@@ -0,0 +1,14 @@
+list_1 = [1, 2, 3, 4, 5, 6]
+#list_1 = [1, 2, 3]
+#list_1 = [1, 2, 3, 4, 5]
+#list_1 = [1]
+#list_1 = []
+if len(list_1) == 0:
+    print([[], []])
+
+middle_index = (len(list_1) + 1) // 2
+
+first_half = list_1[:middle_index]
+second_half = list_1[middle_index:]
+result_list = [first_half, second_half]
+print(result_list)
