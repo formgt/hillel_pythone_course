e3b7595a6be194abaecc8042329b64a3f70bf35e
Margarita <maggie.rudenko@gmail.com>
2025-01-21 17:40:46 -0500

    HW for Lesson_9

---
+++ Lesson_9/HW_9.2.py
@@ -0,0 +1,16 @@
+def difference(*args):
+    if not args:
+        return 0
+    return round(max(args) - min(args), 2)
+
+
+def run_tests():
+    assert difference(1, 2, 3) == 2, "Test1"
+    assert difference(5, -5) == 10, "Test2"
+    assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, "Test3"
+    assert difference() == 0, "Test4"
+    print("OK")
+
+
+if __name__ == "__main__":
+    run_tests()
